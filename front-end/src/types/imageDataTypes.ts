80fa4e8b9e109f3646da777e1c3d0a049055f63e
TomerDun <tomer@dunsky.name>
2025-10-23 14:05:36 +0300

    added types for image data

---
+++ front-end/src/types/imageDataTypes.ts
@@ -0,0 +1,19 @@
+export type ImageDataResponse = {
+    id: number;
+    tc_image_id: string;
+    raw_image_path: string;
+    created_at: string;
+    intensity_average: number;
+    classification_label: string;
+    processed_image_path: string | null;
+    histogram_path: string | null;
+    focus_score: number;
+}
+
+export type ImageMetadata = {    
+    tc_image_id: string;
+    created_at: string;
+    intensity_average: number;
+    classification_label: string;
+    focus_score: number;
+}
