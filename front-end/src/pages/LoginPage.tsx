762736a4942191c8faa95c4380f97c5c5e7ed8b2
TomerDun <tomer@dunsky.name>
2025-10-23 21:41:02 +0300

    added some login styling

--- front-end/src/pages/LoginPage.tsx
+++ front-end/src/pages/LoginPage.tsx
@@ -29,10 +29,10 @@ export default function LoginPage() {
     return (
         <div id="login-page-container" className="w-full h-full flex justify-center items-center">
             <div id="login-form" className="w-[30%] flex flex-col items-center gap-5 bg-white p-8 rounded-lg shadow-md">
-                <h1 className="text-2xl font-semibold">Login</h1>
+                <h1 className="text-xl font-semibold mb-10 text-gray-700">Please login to use the system</h1>
 
-                <input type="text" name="username" id="username" value={usernameInput} onChange={e => setUsernameInput(e.target.value)} placeholder="username" className="border shadow-sm border-gray-300 p-1 rounded-md" />
-                <input type="password" name="password" id="password" value={passwordInput} onChange={e => setPasswordInput(e.target.value)} placeholder="password" className="border border-gray-300 shadow-sm p-1 rounded-md" />
+                <input type="text" name="username" id="username" value={usernameInput} onChange={e => setUsernameInput(e.target.value)} placeholder="username" className="border shadow-sm border-gray-300 p-1 rounded-md w-[70%] text-gray-600" />
+                <input type="password" name="password" id="password" value={passwordInput} onChange={e => setPasswordInput(e.target.value)} placeholder="password" className="border border-gray-300 shadow-sm p-1 rounded-md w-[70%] text-gray-600" />
 
                 <div id="button-row" className="mt-12 w-full">
                     <button onClick={onSubmit} className=" w-full bg-transparent text-blue-600 border border-blue-600 transition-all cursor-pointer hover:bg-gradient-to-r from-blue-500 to-blue-600 hover:text-white px-4 py-2 rounded hover:bg-blue-700">
